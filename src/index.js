fc74fb4ce25b7e102e9a5d382503aad472daa5e3
Junior Dugue <jrdugue@gmail.com>
2019-09-12 12:19:22 -0400

    added css styling to the todo list
    added css styling to the todo list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Todo from "./components/Todo";
 import "./styles.css";
@@ -8,28 +8,29 @@ import "./styles.css";
 // }
 
 function App() {
-
   const [todos, setTodos] = useState([
     {
       text: "Learn Hooks",
-      isCompleted: false,
+      isCompleted: false
     },
     {
       text: "Catch up in React",
-      isCompleted: false,
+      isCompleted: false
     },
     {
       text: "Ace the sprint challenge this weekend!",
-      isCompleted: false,
-    },
+      isCompleted: false
+    }
   ]);
-  
+
   return (
     <div className="App">
-      <h1>ToDo List</h1>
-      {todos.map((todo, index) => (
-        <Todo key={index} index={index} todo={todo} />
-      ))}
+      <div className="todo-list">
+        <h1>ToDo List</h1>
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 }
